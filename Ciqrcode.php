3f0a94c97699ba61c48cab4f34af8f2526830160
Dwi Setiyadi <dwi.setiyadi@gmail.com>
2015-11-29 19:19:49 +0700

    Prevent multiple call of original library

--- Ciqrcode.php
+++ Ciqrcode.php
@@ -20,6 +20,18 @@ class Ciqrcode
 	var $size = 1024;
 	
 	function __construct($config = array()) {
+		// call original library
+		include "qrcode/qrconst.php";
+		include "qrcode/qrtools.php";
+		include "qrcode/qrspec.php";
+		include "qrcode/qrimage.php";
+		include "qrcode/qrinput.php";
+		include "qrcode/qrbitstream.php";
+		include "qrcode/qrsplit.php";
+		include "qrcode/qrrscode.php";
+		include "qrcode/qrmask.php";
+		include "qrcode/qrencode.php";
+		
 		$this->initialize($config);
 	}
 	
@@ -52,18 +64,6 @@ class Ciqrcode
 		
 		// maximum allowed png image width (in pixels), tune to make sure GD and PHP can handle such big images
 		if (!defined('QR_PNG_MAXIMUM_SIZE')) define('QR_PNG_MAXIMUM_SIZE',  $this->size);
-	
-		// call original library
-		include "qrcode/qrconst.php";
-		include "qrcode/qrtools.php";
-		include "qrcode/qrspec.php";
-		include "qrcode/qrimage.php";
-		include "qrcode/qrinput.php";
-		include "qrcode/qrbitstream.php";
-		include "qrcode/qrsplit.php";
-		include "qrcode/qrrscode.php";
-		include "qrcode/qrmask.php";
-		include "qrcode/qrencode.php";
 	}
 	
 	public function generate($params = array()) {
